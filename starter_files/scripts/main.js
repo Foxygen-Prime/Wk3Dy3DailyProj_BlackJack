f2fb558c2c57d4c3e0e9402b636b2c80c2cd6602
Courtney <courtney.trochim@gmail.com>
2017-08-02 14:20:14 -0500

    Creating some logic and algs to test

--- starter_files/scripts/main.js
+++ starter_files/scripts/main.js
@@ -5,13 +5,36 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
+//notes:
+// parseint function is: parseInt(string, radix);//
 
+function handValue(hand) {
+  console.log(hand);
+  let total = 0;
+  for (let i = 0; i < hand.length; i++) {
+    if (hand[i] === "K" || hand[i] === "Q" || hand[i] === "J") {
+      total += 10;
+    }
+    for (let j = 0; j < hand[i].length; j++) {
+      if total >= 21 {
+        let "A" = 1;
+        else {
+          let "A" = 11;
+        }
+        let total = hand[j] +=
+      }
 
-function handValue (hand) {
+    }
 
+    console.log();
+
+    // // if (hand[i] === "A"){
+    //
+    // }
+  }
 
   return;
 }
